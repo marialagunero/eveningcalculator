f1fe297bf02be04e6be91c27706a52bb59a2423d
Maria Lagunero <maria58@gmail.com>
2019-11-14 09:11:07 -0800

    add changes to layout/lookout to scripts, added bmi functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,28 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1, number2);
+alert(result);
+
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
 
 var divide = function(number1, number2) {
   return number1 / number2;
@@ -16,20 +31,15 @@ var divide = function(number1, number2) {
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
-alert(result);
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
 var result = divide(number1, number2);
+alert(result);
+
+
+var bmi = function(weight,height) {
+  return weight / height;
+};
+
+var weight = parseInt(prompt("What is your weight?"));
+var height = parseInt(prompt("What is your heght?"));
+var result = bmi(weight,height);
 alert(result);
